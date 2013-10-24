caa9c26a3afe0a577f84f044ec4b8fc5b081ea5d
jstilwell <jerstil93@gmail.com>
2013-10-24 12:39:10 -0400

    Jeremy Commit

---
+++ src/main/java/edu/wcu/RTPandRTSPStreamingVideo/App.java
@@ -0,0 +1,13 @@
+package edu.wcu.RTPandRTSPStreamingVideo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/edu/wcu/RTPandRTSPStreamingVideo/VideoInterface.java
@@ -0,0 +1,18 @@
+package edu.wcu.RTPandRTSPStreamingVideo;
+
+import java.io.IOException;
+
+public interface VideoInterface {
+
+    /** Size of the header field before each frame of video in an MJPEG file */ 
+    public static final int HEADER = 5;
+
+    /**
+     * Get the next frame from a file.
+     * @param frame an array of bytes that will contain the actual frame upon
+     * method completion.
+     * @return size of the frame
+     * @throws IOException if we cannot read from the video file.
+     */
+    public int getNextFrame(byte[] frame) throws IOException;
+}
