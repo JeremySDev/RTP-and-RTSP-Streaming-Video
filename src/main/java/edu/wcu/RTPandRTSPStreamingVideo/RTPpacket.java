db28008c5783d1d09bf8a418d8d9d8aff2b8e185
jstilwell <jerstil93@gmail.com>
2013-10-24 23:03:03 -0400

    Jeremy Commit

--- src/main/java/edu/wcu/RTPandRTSPStreamingVideo/RTPpacket.java
+++ src/main/java/edu/wcu/RTPandRTSPStreamingVideo/RTPpacket.java
@@ -11,59 +11,50 @@ public class RTPpacket {
     //--------------------------
     // Construct an  RTPpacket object from header fields and payload bitstream
     //--------------------------
-    public RTPpacket(int pType, int framenb, int time, byte[] data,
-            int data_length)
-    {
-        //fill by default header fields:
+  public RTPpacket(int pType, int framenb, int time, byte[] data, 
+                   int data_length){
+    //fill by default header fields:
 
-        //fill changing header fields:
+    //fill changing header fields:
+    
+    //build the header bitstream:
 
-        //build the header bitstream:
+    //fill the RTP header and payload
+  }
+    
+  //--------------------------
+  //Constructor of an RTPpacket object from the packet bitsream 
+  //--------------------------
+  public RTPpacket(byte[] packet, int packet_size) {
+    //fill default fields:
 
-        //fill the RTP header and payload
-    }
+    //fill changing header fields:
 
-    //--------------------------
-    //Constructor of an RTPpacket object from the packet bitsream
-    //--------------------------
-    public RTPpacket(byte[] packet, int packet_size)
-    {
-        //fill default fields:
+    //build the header bitstream:
 
-        //fill changing header fields:
+    //fill the RTP header and payload
+ }
 
-        //build the header bitstream:
-
-        //fill the RTP header and payload
-    }
-
-    //--------------------------
-    //print headers without the SSRC
-    //--------------------------
-    public void printHeader()
-    {
+  //--------------------------
+  //print headers without the SSRC
+  //--------------------------
+    public void printHeader() {
         for (int i = 0; i < (HEADER_SIZE - 4); i++) {
-            for (int j = 7; j >= 0; j--) {
-                if (((1 << j) /*& header[i]*/) != 0) {
+            for (int j = 7; j >= 0; j--)
+                if (((1 << j) & header[i] ) != 0)
                     System.out.print("1");
-                }
-                else {
+                else
                     System.out.print("0");
-                }
-            }
             System.out.print(" ");
         }
         System.out.println();
     }
 
-    //return the unsigned value of 8-bit integer nb
-    static int unsignedInt(int num)
-    {
-        if (num >= 0) {
-            return (num);
-        }
-        else {
-            return (256 + num);
-        }
+  //return the unsigned value of 8-bit integer nb
+    static int unsignedInt(int num) {
+        if (num >= 0)
+            return(num);
+        else
+            return(256 + num);
     }
 }
--- src/main/java/edu/wcu/RTPandRTSPStreamingVideo/VideoInterface.java
+++ src/main/java/edu/wcu/RTPandRTSPStreamingVideo/VideoInterface.java
@@ -4,16 +4,13 @@ import java.io.IOException;
 
 public interface VideoInterface {
 
-    /**
-     * Size of the header field before each frame of video in an MJPEG file
-     */
+    /** Size of the header field before each frame of video in an MJPEG file */ 
     public static final int HEADER = 5;
 
     /**
      * Get the next frame from a file.
-     *
      * @param frame an array of bytes that will contain the actual frame upon
-     *              method completion.
+     * method completion.
      * @return size of the frame
      * @throws IOException if we cannot read from the video file.
      */
