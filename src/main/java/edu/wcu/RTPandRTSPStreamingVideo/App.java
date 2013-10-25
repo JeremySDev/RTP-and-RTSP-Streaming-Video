7153b58f1d23f862efd619639ffda8c09ae28b2b
jstilwell <jerstil93@gmail.com>
2013-10-24 22:56:45 -0400

    Jeremy Commit

--- src/main/java/edu/wcu/RTPandRTSPStreamingVideo/App.java
+++ src/main/java/edu/wcu/RTPandRTSPStreamingVideo/App.java
@@ -2,12 +2,10 @@ package edu.wcu.RTPandRTSPStreamingVideo;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
--- src/main/java/edu/wcu/RTPandRTSPStreamingVideo/Server.java
+++ src/main/java/edu/wcu/RTPandRTSPStreamingVideo/Server.java
@@ -29,6 +29,7 @@ import javax.swing.JFrame;
 /**
  * Models an RTSP server.
  * usage: java Server <RTSP port>
+ *
  * @author William Kreahling, based on Kurose/Ross
  * @version October 11, 2013
  */
@@ -40,52 +41,79 @@ public class Server extends Stream {
     }
 
     // Packet:
-    /** UDP packet containing the video frames  */
+    /**
+     * UDP packet containing the video frames
+     */
     private DatagramPacket senddp;
-    /** Client IP address                       */
-    private InetAddress     ClientIPAddr; 
-    /** Destination port for RTP packets        */
-    private int             rtpDestPort;    
+    /**
+     * Client IP address
+     */
+    private InetAddress ClientIPAddr;
+    /**
+     * Destination port for RTP packets
+     */
+    private int rtpDestPort;
 
     //UI:
-    /** Main UI window          */
+    /**
+     * Main UI window
+     */
     private JFrame frame;
-    /** Label to display text   */
+    /**
+     * Label to display text
+     */
     private JLabel label;
 
     //Video:
-    /** ID number for the image currently being transmitted */
-    private int         imageNum; 
-    /** Access video frames                                 */
-    private VideoStream video; 
+    /**
+     * ID number for the image currently being transmitted
+     */
+    private int imageNum;
+    /**
+     * Access video frames
+     */
+    private VideoStream video;
 
-    /** RTP payload type for MJPEG video            */
-    public final static int MJPEG_TYPE      = 26;  
-    /** Frame period of the video to stream, in ms  */
-    public final static int FRAME_PERIOD    = 100; 
-    /** Length of the video in frames               */
-    public final static int VIDEO_LENGTH    = 3000; 
+    /**
+     * RTP payload type for MJPEG video
+     */
+    public final static int MJPEG_TYPE = 26;
+    /**
+     * Frame period of the video to stream, in ms
+     */
+    public final static int FRAME_PERIOD = 100;
+    /**
+     * Length of the video in frames
+     */
+    public final static int VIDEO_LENGTH = 3000;
 
-    /** Scanner connected to the input stream of the RTSP socket */
-    private Scanner         scanIn;
-    /** Scanner connected to the output stream of the RTSP socket */
-    private BufferedWriter  scanOut;
+    /**
+     * Scanner connected to the input stream of the RTSP socket
+     */
+    private Scanner scanIn;
+    /**
+     * Scanner connected to the output stream of the RTSP socket
+     */
+    private BufferedWriter scanOut;
 
-    /** 
+    /**
      * Constructor that creates a server that listens an RTSP client.
+     *
      * @param portNum the listening port number.
      * @throws NumberFormatException thrown if port number is incorrectly
-     * formatted.
-     * @throws IOException thrown if an error occurs creating the socket.
+     *                               formatted.
+     * @throws IOException           thrown if an error occurs creating the
+     * socket.
      */
     public Server(String portNum) throws NumberFormatException,
-                                         IOException {
+            IOException
+    {
         createUI();
         int rtspPort = 0;
-   
+
         // Initiate TCP connection with the client for the RTSP session
-        rtspPort                    = Integer.parseInt(portNum);
-        ServerSocket listenSocket   = new ServerSocket(rtspPort);
+        rtspPort = Integer.parseInt(portNum);
+        ServerSocket listenSocket = new ServerSocket(rtspPort);
         setRtspSocket(listenSocket.accept());
         listenSocket.close();
 
@@ -96,8 +124,8 @@ public class Server extends Stream {
 
         // Initialize input and output streams
         scanIn = new Scanner(getRtspSocket().getInputStream());
-        scanOut = new BufferedWriter(new 
-            OutputStreamWriter(getRtspSocket().getOutputStream()) );
+        scanOut = new BufferedWriter(new
+                OutputStreamWriter(getRtspSocket().getOutputStream()));
 
     }
 
@@ -105,7 +133,8 @@ public class Server extends Stream {
      * Create the server's UI. It is kind of pathetic, but the server only has
      * a UI because this is a learning experience.
      */
-    private void createUI() {
+    private void createUI()
+    {
 
         frame = new JFrame("Server");
         setRtspID(123456);  // Not really used, right now, maybe someday!
@@ -113,11 +142,13 @@ public class Server extends Stream {
 
         // Handler to close the main window
         frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
+            public void windowClosing(WindowEvent e)
+            {
                 // Stop the timer and exit
                 stopTimer();
                 System.exit(0);
-            }});
+            }
+        });
 
         // Populate and visibilize the UI:
         label = new JLabel("Send frame #        ", JLabel.CENTER);
@@ -129,26 +160,31 @@ public class Server extends Stream {
 
     /**
      * Create a new video object connected to the movie!
+     *
      * @throws IOException if we cannot create the object for some reason.
      */
-    public void setVideo() throws IOException {
+    public void setVideo() throws IOException
+    {
         this.video = new VideoStream(getVideoFileName());
     }
 
     /**
-     *  Print a usage message and end the program.
+     * Print a usage message and end the program.
      */
-    public static void printUsageAndExit() {
+    public static void printUsageAndExit()
+    {
         System.out.println("usage: java Server <RTSP port>");
         System.exit(1);
 
     }
-          
+
     /**
      * Entry point into the program. Whee
+     *
      * @param args the port number for this server.
      */
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
 
         if (args.length != 1) {
             Server.printUsageAndExit();
@@ -157,7 +193,8 @@ public class Server extends Stream {
         Server server = null;
         try {
             server = new Server(args[0]);
-        } catch (IOException  | NumberFormatException ex) {
+        }
+        catch (IOException | NumberFormatException ex) {
             System.out.println("Error creating the server: " + ex.getMessage());
             System.exit(2);
         }
@@ -165,21 +202,22 @@ public class Server extends Stream {
         // Wait for the SETUP message from the client, good server!
         Message requestType = Message.PLAY;
         boolean done = false;
-        while (!done) {
+        while (! done) {
             requestType = server.parseRtspRequest(); // blocking call
-    
+
             if (requestType == Message.SETUP) {
                 done = true;
                 server.setReadyState();
-   
+
                 // Create VideoStream
                 try {
                     server.sendRtspResponse();
                     server.setVideo();
                     server.setRtpSocket(new DatagramSocket());
-                } catch (IOException ioe) {
+                }
+                catch (IOException ioe) {
                     System.out.println("Error communicating with the client: " +
-                                       ioe.getMessage());
+                            ioe.getMessage());
                     System.exit(3);
 
                 }
@@ -188,31 +226,34 @@ public class Server extends Stream {
 
         // Loop to handle RTSP requests
         try {
-            while(requestType != Message.TEARDOWN) {
+            while (requestType != Message.TEARDOWN) {
                 requestType = server.parseRtspRequest(); //blocking
-            
+
                 if (requestType == Message.PLAY && server.isReadyState()) {
                     server.sendRtspResponse();
                     server.startTimer();
                     server.setPlayState();
-                } else if (requestType == Message.PAUSE && 
-                           server.isPlayState()) {
+                }
+                else if (requestType == Message.PAUSE &&
+                        server.isPlayState()) {
                     server.sendRtspResponse();
                     server.stopTimer();
                     server.setReadyState();
                 }
             }
-        } catch (IOException ioe) { 
-            System.out.println("IOException caught: " + ioe.getMessage()); 
+        }
+        catch (IOException ioe) {
+            System.out.println("IOException caught: " + ioe.getMessage());
             System.exit(1);
-            
+
         }
         try {
             server.sendRtspResponse();
             server.stopTimer();
             server.getRtspSocket().close();
             server.getRtpSocket().close();
-        } catch (IOException ioe) { 
+        }
+        catch (IOException ioe) {
             // Ignore this because the sockets are in the process of being
             // closed down anyway.
         }
@@ -223,13 +264,14 @@ public class Server extends Stream {
      * Handler for the timer. Tick tock.
      */
     class TimerListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
+        public void actionPerformed(ActionEvent e)
+        {
 
             int imageLength = 1;
             // if the current image number is less than the length of the video
             if (imageNum < VIDEO_LENGTH && imageLength > 0) {
                 imageNum++;
-               
+
                 try {
                     /*
                      * Get the next frame to send from the video, as well as its
@@ -239,13 +281,13 @@ public class Server extends Stream {
 
                     // Build an RTPpacket object containing the frame
                     if (imageLength > 0) {
-                        RTPpacket rtpPacket = new RTPpacket(MJPEG_TYPE, 
-                                                            imageNum, 
-                                                            (imageNum * 
-                                                            FRAME_PERIOD), 
-                                                            getBuffer(), 
-                                                            imageLength);
-                  
+                        RTPpacket rtpPacket = new RTPpacket(MJPEG_TYPE,
+                                imageNum,
+                                (imageNum *
+                                        FRAME_PERIOD),
+                                getBuffer(),
+                                imageLength);
+
                         // Get to total length of the full RTP packet to send
                         int packetLength = rtpPacket.getlength();
                         /* 
@@ -258,19 +300,21 @@ public class Server extends Stream {
                          * Send the packet as a DatagramPacket over the UDP
                          * socket.
                          */
-                        senddp = new DatagramPacket(packetBits, packetLength, 
-                                                    ClientIPAddr, rtpDestPort);
+                        senddp = new DatagramPacket(packetBits, packetLength,
+                                ClientIPAddr, rtpDestPort);
                         getRtpSocket().send(senddp);
 
                         // update UI
                         label.setText("Send frame #" + imageNum);
                     }
-                } catch(Exception ex) {
-                    System.out.println("EXCEPTION caught: "+ex);
+                }
+                catch (Exception ex) {
+                    System.out.println("EXCEPTION caught: " + ex);
                     ex.printStackTrace();
                     System.exit(0);
                 }
-            } else {
+            }
+            else {
                 // If we have reached the end of the video file, stop the timer
                 stopTimer();
             }
@@ -280,36 +324,39 @@ public class Server extends Stream {
     /**
      * Turn a String representation into its enum equivalent.
      * Should the enum itself do this, probably, but Meh.
+     *
      * @param word the string to change into an enumerated type.
      */
-    private Message string2Message(String word) {
+    private Message string2Message(String word)
+    {
         Message result = Message.INVALID;
-        
-        switch(word) { // Requires Java 7
-        case "SETUP":
-            result = Message.SETUP;
-            break;
-        case "PLAY":
-            result = Message.PLAY;
-            break;
-        case "PAUSE":
-            result = Message.PAUSE;
-            break;
-        case "TEARDOWN":
-            result = Message.TEARDOWN;
-            break;
+
+        switch (word) { // Requires Java 7
+            case "SETUP":
+                result = Message.SETUP;
+                break;
+            case "PLAY":
+                result = Message.PLAY;
+                break;
+            case "PAUSE":
+                result = Message.PAUSE;
+                break;
+            case "TEARDOWN":
+                result = Message.TEARDOWN;
+                break;
         }
         return result;
-        
+
     }
 
     /**
      * Figure out what the server sent to us!
      */
-    private Message parseRtspRequest() {
+    private Message parseRtspRequest()
+    {
         // When in doubt kill all the sockets!
         Message requestType = Message.TEARDOWN;
-        try{
+        try {
             // Parse request line and extract the requestType:
             String RequestLine = scanIn.nextLine();
 
@@ -327,7 +374,7 @@ public class Server extends Stream {
             tokens = new StringTokenizer(SeqNumLine);
             tokens.nextToken();
             setRtspSeqNum(Integer.parseInt(tokens.nextToken()));
-    
+
             // Get LastLine
             String LastLine = scanIn.nextLine();
 
@@ -335,29 +382,33 @@ public class Server extends Stream {
                 // Extract rtpDestPort from LastLine
                 tokens = new StringTokenizer(LastLine);
 
-                /** 
+                /**
                  * Format is : "Transport: RTP/UDP; Client_port= portNum"
                  */
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < 3; i++) {
                     tokens.nextToken(); //skip unused stuff
+                }
                 rtpDestPort = Integer.parseInt(tokens.nextToken());
             }
             // else LastLine will be the SessionId line, do not check for now.
-        } catch(NoSuchElementException | IllegalStateException ex) {
+        }
+        catch (NoSuchElementException | IllegalStateException ex) {
             // If this happens we are borked, so quiting now, instead of
             // sending to main!
-            System.out.println("Error Parsing RTSP request: " + 
-                               ex.getMessage());
+            System.out.println("Error Parsing RTSP request: " +
+                    ex.getMessage());
             System.exit(1);
         }
-        return(requestType);
+        return (requestType);
     }
 
     /**
      * Send our response to the request!
+     *
      * @throws IOException if something is wrong with the socket.
      */
-    private void sendRtspResponse() throws IOException {
+    private void sendRtspResponse() throws IOException
+    {
         // TODO
     }
 }
