a6bfbddab28159a1646cd176e368bd0ff2525706
jstilwell <jerstil93@gmail.com>
2013-10-26 16:28:08 -0400

    Jeremy Commit

---
+++ src/main/java/edu/wcu/RTPandRTSPStreamingVideo/VideoStream.java
@@ -0,0 +1,17 @@
+package edu.wcu.RTPandRTSPStreamingVideo;
+
+/**
+ * @author Jeremy Stilwell
+ * @version 10/26/13.
+ */
+public class VideoStream
+{
+    public VideoStream(String videoFileName)
+    {
+    }
+
+    public int getNextFrame(byte[] buffer)
+    {
+        return 0;
+    }
+}
