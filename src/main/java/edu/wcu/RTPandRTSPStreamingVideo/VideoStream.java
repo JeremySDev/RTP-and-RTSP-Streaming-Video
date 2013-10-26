e7575992c2ae9ddb206ece6e629c8dc7d2d5e1d6
jstilwell <jerstil93@gmail.com>
2013-10-26 16:37:17 -0400

    Jeremy Commit

--- src/main/java/edu/wcu/RTPandRTSPStreamingVideo/VideoStream.java
+++ src/main/java/edu/wcu/RTPandRTSPStreamingVideo/VideoStream.java
@@ -4,7 +4,7 @@ package edu.wcu.RTPandRTSPStreamingVideo;
  * @author Jeremy Stilwell
  * @version 10/26/13.
  */
-public class VideoStream
+public class VideoStream implements VideoInterface
 {
     public VideoStream(String videoFileName)
     {
